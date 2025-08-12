64a530875b0637212c9f29135fde3dce25526ace
Philip William Ventura <philipwilliamventura@gmail.com>
2025-08-12 19:52:18 -0400

    Integrated Clerk and Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CompanionCard from '@/components/CompanionCard'
 import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
@@ -9,13 +10,38 @@ const Page = () => {
       <h1 className='text-2xl underline'>Popular Companions</h1>
 
       <section className='home-section'>
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        <CompanionCard 
+          id = "123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject = "science"
+          duration = {45}
+          color = "#ffda6e"
+        />
+        <CompanionCard 
+          id = "456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives and Integrals"
+          subject = "maths"
+          duration = {30}
+          color = "#e5d0ff"
+        />
+        <CompanionCard 
+          id = "789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject = "English Literature"
+          duration = {30}
+          color = "#BDE7FF"
+        />
       </section>
 
       <section className='home-section'>
-        <CompanionsList />
+        <CompanionsList 
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
         <CTA />
       </section>
     </main>
