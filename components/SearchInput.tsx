2bbc29acacab40e75472343ab1fc6a5a8f1981f0
Philip William Ventura <philipwilliamventura@gmail.com>
2025-08-13 12:09:11 -0400

    Implemented Library

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { formUrlQuery, removeKeysFromUrlQuery } from '@jsmastery/utils';
 import Image from 'next/image';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react';
@@ -13,9 +14,26 @@ const SearchInput = () => {
   const [searchQuery, setsearchQuery] = useState('')
 
   useEffect(() => {
-    if(searchQuery) {
-        router.push(`/currentRoute?topic=${searchQuery}`)
-    }
+    const delayDebounceFn = setTimeout(() => {
+        if(searchQuery) {
+        const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "topic",
+        value: searchQuery,
+        });
+
+        router.push(newUrl, { scroll: false });
+        } else {
+            if(pathname === "/companions") {
+                const newUrl = removeKeysFromUrlQuery({
+                params: searchParams.toString(),
+                keysToRemove: ["topic"],
+                });
+
+                router.push(newUrl, { scroll: false });
+            }
+        }
+    }, 500)
   }, [searchQuery, router, searchParams, pathname]);
 
   return (
